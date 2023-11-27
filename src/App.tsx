f07fa5361bb586ed9b57d70c243d8c50c0352356
Milan Poudel <milanwebdeveloper1@gmail.com>
2023-11-27 18:28:34 +0545

    setup/project setup with tailwind and vite

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./index.css";
+
+function App() {
+  return <div className="text-3xl font-bold underline">Hello my friend</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.jsx", "./src/**/*.jsx", "./src/**/*.ts"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
